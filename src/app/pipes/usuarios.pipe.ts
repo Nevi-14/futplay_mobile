e2cab308d103ec5591cf53ba807e449f02bbeb29
Nelson Morera <workemailnelson@gmail.com>
2021-11-06 19:36:22 -0600

    20211106V1.1.5

--- src/app/pipes/usuarios.pipe.ts
+++ src/app/pipes/usuarios.pipe.ts
@@ -8,14 +8,15 @@ import { UserService } from '../services/user.service';
 export class UsuariosPipe implements PipeTransform {
   constructor(private user: UserService) { }
 
-  transform(value: any): any {
+  transform(texto: string = '',
+  columna: string): any {
     let usuarios: Usuario[] = [];
 let user = 'usuarioID';
-
+console.log('columna',columna)
     usuarios = this.user.user;
-    const usuario = usuarios.find( d => d[user] === value );
+    const usuario = usuarios.find( d => d[user] === texto );
     if ( usuario !== undefined){
-      return usuario.nombre +' ' + usuario.apellido1;
+      return usuario[columna];
     } else {
       return 'ND';
     }
