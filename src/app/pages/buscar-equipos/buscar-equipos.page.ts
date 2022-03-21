b370892c614023679f4aa86d613bd7b1837cbbcb
Nelson Morera <workemailnelson@gmail.com>
2022-03-20 18:24:44 -0600

    20220320v1.6

--- src/app/pages/buscar-equipos/buscar-equipos.page.ts
+++ src/app/pages/buscar-equipos/buscar-equipos.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { EquiposService } from 'src/app/services/equipos.service';
+import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
   selector: 'app-buscar-equipos',
@@ -10,10 +12,13 @@ export class BuscarEquiposPage implements OnInit {
 
   stadiumProfile =  'assets/main/game-match.svg';
   constructor(
-    public modalCtrl: ModalController
+    public modalCtrl: ModalController,
+    public equiposService: EquiposService,
+    public usuariosService:UsuariosService
   ) { }
 
   ngOnInit() {
+    this.equiposService.SyncEquipos(this.usuariosService.usuarioActual.Cod_Usuario);
   }
 
   cerrarModal(){
--- src/app/pages/buscar-jugadores/buscar-jugadores.page.ts
+++ src/app/pages/buscar-jugadores/buscar-jugadores.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { UsuariosService } from '../../services/usuarios.service';
 
 @Component({
   selector: 'app-buscar-jugadores',
@@ -10,10 +11,13 @@ export class BuscarJugadoresPage implements OnInit {
 
   stadiumProfile =  'assets/main/game-match.svg';
   constructor(
-    public modalCtrl: ModalController
+    public modalCtrl: ModalController,
+    public usuariosService: UsuariosService
   ) { }
 
   ngOnInit() {
+    this.usuariosService.syncUsusarios();
+
   }
 
   cerrarModal(){
